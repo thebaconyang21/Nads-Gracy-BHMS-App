56082880d21705725974312c37701424e365b517
Beah <beahpayot21@gmail.com>
2026-09-11 15:36:12 +0000

    Correct the SUPABASE_URL

--- services/supabase.ts
+++ services/supabase.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const SUPABASE_URL = 'Ykmuvrfrokykbczrywtki';
+const SUPABASE_URL = 'https://kmuvfrfokykbcrzywtki.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_JFCI0-aHOF1TDi-vupFOSg_pGxbyvx9';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
