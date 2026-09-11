1b6bd81081a19db92553b19435691ce4bb94d48a
Beah <beahpayot21@gmail.com>
2026-09-11 15:03:05 +0000

    configuration code

---
+++ services/supabase.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
+
+const SUPABASE_URL = 'YOUR_SUPABASE_URL_HERE';
+const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY_HERE';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionUrl: false,
+  },
+});
